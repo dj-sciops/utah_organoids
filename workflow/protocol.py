14bb6868ad247e551c71d51e94b8ae224f1580c7
Tolga Dincer <tolgadincer@gmail.com>
2022-10-07 11:47:45 -0500

    first iteration

--- workflow/protocol.py
+++ workflow/protocol.py
@@ -6,7 +6,8 @@ if "custom" not in dj.config:
 
 db_prefix = dj.config["custom"].get("database.prefix", "")
 
-schema = dj.schema(db_prefix + 'protocol')
+schema = dj.schema(db_prefix + "protocol")
+
 
 @schema
 class User(dj.Manual):
@@ -14,71 +15,135 @@ class User(dj.Manual):
     user: varchar(32)
     """
 
+
+# Move to another schema
 @schema
 class Family(dj.Manual):
-     definition = """
-     family: varchar(8)
-     """
+    definition = """
+    family: varchar(8)
+    """
 
+
+# Move to another schema
 @schema
 class Line(dj.Manual):
-     definition = """
-     line: varchar(8)
-     """
+    definition = """
+    line: varchar(8)
+    """
 
+
+@schema
+class Passage(dj.Manual):
+    definition = """
+    passage_no: int
+    -> Line
+    """
+
+
+@schema
+class Supplement(dj.Lookup):
+    definition = """
+    supplement: varchar(32)
+    concentration: int # in units of micromolar or ng/mL
+    """
+
+
+# Move to another schema
 @schema
 class Induction(dj.Manual):
-     definition = """
-     code: varchar(8)
-     ---
-     -> Family
-     -> Line
-     """
+    definition = """
+    code: varchar(8)
+    ---
+    -> Family
+    -> Passage
+    """
 
-     class Sequence(dj.Part):
-          definition = """
-          -> master
-          sequence: varchar(8)
-          """
+    class Sequence(dj.Part):
+        definition = """
+        -> master
+        sequence: varchar(8)
+        """
+
 
 @schema
+class Media(dj.LookUp):
+    definition = """
+    media: varchar(32)
+    """
+
+
+@schema
+class Substrate(dj.LookUp):
+    definition = """
+    substrate: varchar(32)
+    """
+
+
+# new schema
+@schema
 class InductionNotes(dj.Manual):
-     definition = """
-     -> Induction
-     note_datetime: datetime
-     ---
-     induction_step=null: enum('ipsc_start', 'ipsc_replate', 'induction_start')
-     induction_note=null: varchar(256)
-     """
+    definition = """
+    -> Induction
+    note_datetime: datetime
+    ---
+    media_change: bool
+    induction_step=null: enum('ipsc_thaw', 'ipsc_replate', 'induction_start')
+    induction_note=null: varchar(256)
+    density=null: int # in percentage in units
+    """
+
+    class InductionParameters(dj.Part):
+        definition = """
+        -> master
+        ---
+        -> Supplement
+        -> Media
+        -> Substrate
+        """
+
+
+# Images will be stored with "code_datetime" naming convention.
+
+
+@schema
+class InductionImages(dj.Manual):
+    definition = """
+    -> InductionNotes
+    ---
+    directory: varchar(256)
+    """
+
 
 @schema
 class Organoid(dj.Manual):
-     definition = """
-     organoid_id: int
-     """
+    definition = """
+    organoid_id: int
+    """
+
 
 @schema
 class OrganoidFamily(dj.Manual):
-     definition = """
-     -> Organoid
-     -> Family
-     """
+    definition = """
+    -> Organoid
+    -> Family
+    """
+
 
 @schema
 class Plate(dj.Manual):
-     definition = """
-     plate_id: varchar(8)
-     ---
-     dish: varchar(8)
-     code1: varchar(8)
-     code2: varchar(32)
-     sr: date
-     -> Line
-     -> User
-     """
+    definition = """
+    plate_id: varchar(8)
+    ---
+    dish: varchar(8)
+    code1: varchar(8)
+    code2: varchar(32)
+    sr: date
+    -> Line
+    -> User
+    """
 
-     class OrganoidPlate(dj.Part):
-          definition = """
-          -> master
-          -> Organoid
-          """
+    class OrganoidPlate(dj.Part):
+        definition = """
+        -> master
+        -> Organoid
+        """
