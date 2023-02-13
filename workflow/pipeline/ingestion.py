8bb96ffeab3ac553e01c389d1dd9923631d48c0f
JaerongA <jaerong.ahn@datajoint.com>
2023-02-13 17:07:08 -0600

    add downsampling functions

--- workflow/pipeline/ingestion.py
+++ workflow/pipeline/ingestion.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Any
+from typing import Any, Iterator
 
 import datajoint as dj
 import numpy as np
@@ -185,3 +185,50 @@ def get_probe_info(session_key: dict[str, Any]) -> dict[str, Any]:
 
     with open(probe_meta_file, "r") as f:
         return yaml.safe_load(f)
+
+
+def array_generator(arr: np.array, chunk_size: int = 10) -> Iterator[np.array]:
+    """Generates an array at a specified chunk
+
+    Args:
+        arr (np.array): 1d numpy array
+        chunk_size (int, optional): Size of the output array. Defaults to 10.
+
+    Yields:
+        Iterator[np.array]: generator object
+    """
+    start_ind = end_ind = 0
+    while end_ind < arr.shape[0]:
+        end_ind += chunk_size
+        new_arr = arr[start_ind:end_ind]
+
+        yield new_arr
+        start_ind += chunk_size
+
+
+def downsample_arr(
+    arr: np.array, ds_factor: int = 10, selected_ind: int = 0
+) -> np.array:
+    """Function for downsampling an array
+
+    Args:
+        arr (np.array): 1d numpy array
+        ds_factor (int, optional): Downsampling factor. Defaults to 10. If 10, 20kHz will be downsampled to 2kHz.
+        selected_ind (int, optional): Select which index to keep. Defaults to 0 (first index).
+
+    Returns:
+        np.array: Final downsampled array.
+    """
+    if arr.shape[0] < ds_factor:
+        raise NotImplementedError("Array is smaller than the downsampling factor")
+
+    assert selected_ind < ds_factor, "selected_ind should be < ds_factor"
+
+    downsampled_arr = []
+
+    for new_arr in array_generator(arr, chunk_size=ds_factor):
+        if new_arr.shape[0] <= selected_ind:
+            selected_ind = -1
+        downsampled_arr.append(new_arr[selected_ind])
+
+    return np.array(downsampled_arr)
