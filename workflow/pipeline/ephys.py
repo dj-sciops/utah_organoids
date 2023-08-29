8e347208212d32adb5401bbef2a690d591f930c2
Kabilar Gunalan <kabilar@datajoint.com>
2023-08-29 15:53:49 -0500

    Updates for schema and table renames

--- workflow/pipeline/ephys.py
+++ workflow/pipeline/ephys.py
@@ -4,7 +4,7 @@ from element_array_ephys import probe
 
 from workflow import DB_PREFIX
 
-from workflow.pipeline import induction
+from workflow.pipeline import culture
 from workflow.utils.paths import (
     get_ephys_root_data_dir,
     get_processed_root_data_dir,
@@ -15,7 +15,7 @@ __all__ = ["ephys", "probe"]
 
 
 if not ephys.schema.is_activated():
-    Subject = induction.OrganoidExperiment
+    Subject = culture.Experiment
     EPHYS_STORE = "ephys-store"
     FILE_STORE = "data-root"
     ephys.activate(DB_PREFIX + "ephys", DB_PREFIX + "probe", linking_module=__name__)
