579022c30bdade3b391cd435943cea85786cbf0a
JaerongA <jaerong.ahn@datajoint.com>
2023-11-17 17:53:09 -0600

    fix El2ROW

--- src/workflow/utils/ingestion_utils.py
+++ src/workflow/utils/ingestion_utils.py
@@ -18,7 +18,7 @@ from workflow.utils.paths import get_raw_root_data_dir, get_repo_dir
 El2ROW = [20, 5, 19, 6, 18, 7, 32, 9, 31, 10, 30, 11, 21, 4, 22, 3, 23, 2, 25, 16, 26, 15, 27, 14, 28, 13, 24, 1, 29, 12, 17, 8]  # array of row number for each electrode obtained from el2row.m
 # fmt: on
 
-electrode_to_row = np.array(El2ROW) - 1  # 0-based indexing
+El2ROW = np.array(El2ROW) - 1  # 0-based indexing
 
 
 def get_channel_to_electrode_map(port_id: str | None = None) -> dict[str, int]:
@@ -32,12 +32,10 @@ def get_channel_to_electrode_map(port_id: str | None = None) -> dict[str, int]:
     """
     if port_id in ["A", "B", "C", "D"]:
         channel_to_electrode_map = {
-            f"{port_id}-{value:03}": key for key, value in enumerate(electrode_to_row)
+            f"{port_id}-{value:03}": key for key, value in enumerate(El2ROW)
         }
     elif port_id is None:
-        channel_to_electrode_map = {
-            str(value): key for key, value in enumerate(electrode_to_row)
-        }
+        channel_to_electrode_map = {str(value): key for key, value in enumerate(El2ROW)}
     else:
         raise ValueError(f"Invalid port_id: {port_id}")
 
