fca4df0e19d82e7ceed636ed8f49322406ecef8a
JaerongA <jaerong.ahn@datajoint.com>
2024-03-05 12:49:25 -0600

    build: :construction_worker: add spike_sorting_worker to worker.py

--- src/workflow/populate/worker.py
+++ src/workflow/populate/worker.py
@@ -4,7 +4,7 @@ import datajoint as dj
 from datajoint_utilities.dj_worker import DataJointWorker, ErrorLog, WorkerLog
 
 from workflow import DB_PREFIX, SUPPORT_DB_PREFIX, WORKER_MAX_IDLED_CYCLE
-from workflow.pipeline import analysis, ephys
+from workflow.pipeline import analysis, ephys, ephys_sorter
 from workflow.support import ingestion_support
 
 logger = dj.logger
@@ -30,3 +30,19 @@ standard_worker(ingestion_support.FileProcessing)
 standard_worker(ephys.EphysSessionInfo, max_calls=5)
 standard_worker(ephys.LFP, max_calls=5)
 standard_worker(analysis.LFPSpectrogram, max_calls=5)
+
+standard_worker(ephys_sorter.PreProcessing, max_calls=5)
+standard_worker(ephys_sorter.PostProcessing, max_calls=5)
+
+# spike sorting process for GPU involved jobs
+spike_sorting_worker = DataJointWorker(
+    "spike_sorting_worker",
+    worker_schema_name=f"{SUPPORT_DB_PREFIX}workerlog",
+    db_prefix=[DB_PREFIX, SUPPORT_DB_PREFIX],
+    run_duration=-1,
+    max_idled_cycle=WORKER_MAX_IDLED_CYCLE,
+    sleep_duration=30,
+    autoclear_error_patterns=autoclear_error_patterns,
+)
+
+spike_sorting_worker(ephys_sorter.SIClustering, max_calls=6)
