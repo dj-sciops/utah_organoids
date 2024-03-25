60331b83fea8826e7988312b3a53e86783f8c67e
JaerongA <jaerong.ahn@datajoint.com>
2024-03-25 14:27:59 -0500

    add spike_processing_worerk

--- src/workflow/populate/worker.py
+++ src/workflow/populate/worker.py
@@ -1,5 +1,3 @@
-import pathlib
-
 import datajoint as dj
 from datajoint_utilities.dj_worker import DataJointWorker, ErrorLog, WorkerLog
 
@@ -9,16 +7,23 @@ from workflow.support import ingestion_support
 
 logger = dj.logger
 
-__all__ = ["standard_worker", "WorkerLog", "ErrorLog"]
+__all__ = [
+    "standard_worker",
+    "spike_processing_worker",
+    "spike_sorting_worker",
+    "WorkerLog",
+    "ErrorLog",
+]
 
 
 # -------- Define process(s) --------
 autoclear_error_patterns = []
+worker_schema_name = (f"{SUPPORT_DB_PREFIX}workerlog",)
 
 # standard process for non-GPU jobs
 standard_worker = DataJointWorker(
     "standard_worker",
-    worker_schema_name=f"{SUPPORT_DB_PREFIX}workerlog",
+    worker_schema_name,
     db_prefix=[DB_PREFIX, SUPPORT_DB_PREFIX],
     run_duration=-1,
     max_idled_cycle=WORKER_MAX_IDLED_CYCLE,
@@ -30,14 +35,28 @@ standard_worker(ingestion_support.FileProcessing)
 standard_worker(ephys.EphysSessionInfo, max_calls=200)
 standard_worker(ephys.LFP, max_calls=10)
 standard_worker(analysis.LFPSpectrogram, max_calls=10)
+standard_worker(ephys.CuratedClustering, max_calls=5)
+standard_worker(ephys.WaveformSet, max_calls=5)
+standard_worker(ephys.QualityMetrics, max_calls=5)
 
-standard_worker(ephys_sorter.PreProcessing, max_calls=5)
-standard_worker(ephys_sorter.PostProcessing, max_calls=5)
+# spike_sorting process for nonGPU-required jobs
+spike_processing_worker = DataJointWorker(
+    "spike_processing_worker",
+    worker_schema_name,
+    db_prefix=[DB_PREFIX, SUPPORT_DB_PREFIX],
+    run_duration=-1,
+    max_idled_cycle=WORKER_MAX_IDLED_CYCLE,
+    sleep_duration=30,
+    autoclear_error_patterns=autoclear_error_patterns,
+)
+
+spike_processing_worker(ephys_sorter.PreProcessing, max_calls=6)
+spike_processing_worker(ephys_sorter.PostProcessing, max_calls=6)
 
 # spike sorting process for GPU involved jobs
 spike_sorting_worker = DataJointWorker(
     "spike_sorting_worker",
-    worker_schema_name=f"{SUPPORT_DB_PREFIX}workerlog",
+    worker_schema_name,
     db_prefix=[DB_PREFIX, SUPPORT_DB_PREFIX],
     run_duration=-1,
     max_idled_cycle=WORKER_MAX_IDLED_CYCLE,
@@ -46,6 +65,3 @@ spike_sorting_worker = DataJointWorker(
 )
 
 spike_sorting_worker(ephys_sorter.SIClustering, max_calls=6)
-standard_worker(ephys.CuratedClustering, max_calls=5)
-standard_worker(ephys.WaveformSet, max_calls=5)
-standard_worker(ephys.QualityMetrics, max_calls=5)
